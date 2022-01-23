28db24897107981d1891243a91ae12b264f684e6
Ian Scheffler <ian.f.scheffler@gmail.com>
2022-01-23 00:20:08 -0500

    starting VERY simple model

---
+++ sentence_generation/simple_sentence_generator.py
@@ -0,0 +1,12 @@
+# This is designed to be the SIMPLEST possible sentence generator
+# Since it doesn't use any ML techniques
+# Maybe we can use this as a basic demo?
+import random
+import time
+# Below is the 'corpus' (I hesitate to use that word since this is so limited)
+# of words that the model can draw on. The model has five words.
+firstWord = ["A", "The", "One"]
+secondWord = ["apple", "carrot", "rabbit"]
+thirdWord = ["began", "started"]
+fourthWord = ["to cook", "to sink", "to swim"]
+fifthWord = ["quickly", "slowly", "rapidly"]
