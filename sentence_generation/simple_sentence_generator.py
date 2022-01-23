84f75b8643714272debafd298e58f3fee0d84e15
Ian Scheffler <ian.f.scheffler@gmail.com>
2022-01-23 01:06:42 -0500

    revised code

--- sentence_generation/simple_sentence_generator.py
+++ sentence_generation/simple_sentence_generator.py
@@ -5,18 +5,18 @@ import random
 import time
 # Below is the 'corpus' (I hesitate to use that word since this is so limited)
 # of words that the model can draw on. The model has five words.
-firstWord = ["A", "The", "One"]
-secondWord = ["apple", "carrot", "rabbit"]
-thirdWord = ["began", "started"]
-fourthWord = ["to cook", "to sink", "to swim"]
-fifthWord = ["quickly", "slowly", "rapidly"]
+article = ["A", "The", "One"]
+noun = ["apple", "carrot", "rabbit"]
+verb = ["began", "started"]
+verb1 = ["to cook", "to sink", "to swim", "to fly", "to speak"]
+adverb = ["quickly", "slowly", "rapidly"]
 # Now, we randomly choose a word from each list! (Since the list are pre-broken down into
 # related clusters--there are better terms for this--it obviously simplifies things further.)
-a = random.choice(firstWord)
-b = random.choice(secondWord)
-c = random.choice(thirdWord)
-d = random.choice(fourthWord)
-e = random.choice(fifthWord)
+a = random.choice(article)
+b = random.choice(noun)
+c = random.choice(verb)
+d = random.choice(verb1)
+e = random.choice(adverb)
 # Now, we very crudely combine the words together!
 sentence = a + " " + b + " " + c + " " + d + " " + e + "."
 print(sentence)
