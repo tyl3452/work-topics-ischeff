34ba40f499f1f677dd1782a04a73069222c7e37a
Ian Scheffler <Ian.f.scheffler@gmail.com>
2022-02-06 12:57:52 -0500

    Delete app.py
    starting over

--- flask/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask# Import Flask Class, and redirect
-app = Flask(__name__) # Create an Instance
-
-@app.route('/') # Route the Function
-def main(): # Run the function
-    return "Hello, World! This is Ian's first web app!"
-
-app.run(host='0.0.0.0', port=5000, debug=True) # Run the Application (in debug mode)
