5ac5e6206424ddf51f6934625ae6c99578942b82
Ian Scheffler <ian.f.scheffler@gmail.com>
2022-02-26 11:29:40 -0500

    added dict model

--- sentence_generation/markov_sentence_generator.py
+++ sentence_generation/markov_sentence_generator.py
@@ -19,16 +19,16 @@ def input(text):
         # corpus.split("\n")
     return corpus
 
-def build_markov(corpus):
-    corpus.split()#split the string input into a list of words
+def build_markov(corpus, state_size):
+    corpus = corpus.split()#split the string input into a list of words
     markov = {}#create an empty dictionary to hold the model - keys and values are words
-    for i in range(len(corpus)):#should these be reversed?
+    for i in range(state_size, len(corpus)):
         current_word = corpus[i]
-        # previous_words = ' '.join(corpus[i-1])
-        # if previous_words in markov:
-        #      markov[previous_words].append(current_word)
-        # else:
-        #      markov[previous_words] = [current_word]
+        previous_words = ' '.join(corpus[i-state_size:i])
+        if previous_words in markov:
+            markov[previous_words].append(current_word)
+        else:
+            markov[previous_words] = [current_word]
     return markov
 
 #
@@ -55,6 +55,6 @@ def build_markov(corpus):
 if __name__ == "__main__":
     body = input('drake.txt')
     print(body)
-    model = build_markov(body)#this seems to be making a model based on characters, not words
+    model = build_markov(body,2)#this seems to be making a model based on characters, not words
     print(model)
-    print(model[len(model)-1])
+    # print(model[len(model)-1])
