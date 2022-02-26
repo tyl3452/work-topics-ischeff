bf2b7ae0cee2ae815c20a5275eebefdf76fdd815
Ian Scheffler <ian.f.scheffler@gmail.com>
2022-02-26 11:39:32 -0500

    model now works

--- sentence_generation/markov_sentence_generator.py
+++ sentence_generation/markov_sentence_generator.py
@@ -12,6 +12,7 @@
 # print(weather)
 
 #get input - use your own song lyrics!
+import random
 def input(text):
     with open(text) as f:
         # corpus = f.readlines()
@@ -31,30 +32,31 @@ def build_markov(corpus, state_size):
             markov[previous_words] = [current_word]
     return markov
 
-#
-#
-# def build_model(source, state_size):
-#     '''
-#     Given some corpus and a state size, build a Markov chain model (as a dictionary).
-#     '''
-#     source = source.split()
-#     model = {}
-#     for i in range(state_size, len(source)):
-#         current_word = source[i]
-#         previous_words = ' '.join(source[i-state_size:i])
-#         if previous_words in model:
-#             model[previous_words].append(current_word)
-#         else:
-#             model[previous_words] = [current_word]
-#
-#     return model
+def generate_text(model, state_size, min_length):
 
+    #Let's assume that each line starts with a capital letter
+    def get_new_starter():
+        return random.choice([s.split(' ') for s in model.keys() if s[0].isupper()])
+    text = get_new_starter()
 
-# we can assume a line ends with a newline character.
+    i = state_size
+    while True:
+        key = ' '.join(text[i-state_size:i])
+        if key not in model:
+            text += get_new_starter()
+            i += 1
+            continue
+
+        next_word = random.choice(model[key])
+        text.append(next_word)
+        i += 1
+        if i > min_length and text[-1][-1] == '.':
+            break
+    return ' '.join(text)
 
 if __name__ == "__main__":
     body = input('drake.txt')
     print(body)
     model = build_markov(body,2)#this seems to be making a model based on characters, not words
     print(model)
-    # print(model[len(model)-1])
+    print(generate_text(model,2,100))
