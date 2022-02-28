806f71a1aebe3082b6194210a552e59a97250046
Ian Scheffler <ian.f.scheffler@gmail.com>
2022-02-27 22:59:28 -0500

    modified

--- flask/app.py
+++ flask/app.py
@@ -1,9 +1,74 @@
-from flask import Flask
+# from flask import Flask
+# app = Flask(__name__)
+#
+# @app.route('/')
+# @app.route('/index')
+# def index():
+#     return "Hello world! This is Ian's second attempt at a Flask web app."
+#
+# app.run(host='0.0.0.0', port=8000, debug=True)
+
+from flask import Flask, request,session
+from flask import render_template
+
+import random
+
 app = Flask(__name__)
+app.secret_key="something"
 
-@app.route('/')
-@app.route('/index')
+# Example of a route that passes
+# data to a template
+@app.route("/rand")
+def randomnumber():
+  i = random.randrange(100)
+  return render_template("lucky.html",number = i )
+
+# the "root" route
+@app.route("/")
 def index():
-    return "Hello world! This is Ian's second attempt at a Flask web app."
+  return "<h1>Hello World!</h1>"
 
-app.run(host='0.0.0.0', port=8000, debug=True)
+# return a template
+@app.route("/about")
+def about():
+  return render_template("about.html")
+
+# example of static content
+# like an image or including css
+@app.route("/image_css")
+def image_css():
+  return render_template("image_css.html")
+
+@app.route("/form_demo",methods=['GET','POST'])
+def form_demo():
+  # GET is when we just load the page in our browser
+  # POST is when we click the button
+  if request.method=="GET":
+    return render_template("form_demo.html")
+  else:
+    # here we clicked the button
+    # so we can check the form data
+    name = request.form['username']
+    pw = request.form['password']
+    print(name,pw)
+    if pw != "12345":
+      error = "BAD PASSWORD"
+      name=""
+    else:
+      error = ""
+
+    return render_template("form_demo.html",error=error, name=name)
+
+@app.route("/session_demo")
+def session_demo():
+
+  print(session)
+  if 'count' not in session:
+    session['count'] = 1
+  else:
+    session['count'] = session['count'] + 1
+
+  return render_template('session_demo.html',count = session['count'])
+
+
+app.run(host="0.0.0.0",port=5000,debug=True)
