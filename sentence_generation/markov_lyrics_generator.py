b90757fc7d18fda6c31562418e5278128501eb96
Ian Scheffler <ian.f.scheffler@gmail.com>
2022-03-06 18:25:28 -0500

    added code

--- sentence_generation/markov_lyrics_generator.py
+++ sentence_generation/markov_lyrics_generator.py
@@ -8,14 +8,14 @@ def input(text):
 
 # This function takes as inputs your corpus (a string of text) and the state size and returns a dictionary that is a Markov model of the corpus
 
-    # Here, state size is another way of saying the "n" in "n-gram": 
-    
+    # Here, state size is another way of saying the "n" in "n-gram":
+
         # If state size is 1, the model will build a dictionary in which each word in the corpus
-        # is a key, and the words that follows a given word--anywhere in the corpus--are the values. 
-        
+        # is a key, and the words that follows a given word--anywhere in the corpus--are the values.
+
         # If state size is 2, the model will build a dictionary in which each pair of words in the corpus
-        # is a key, and the words that follows that pair are the keys. 
-        
+        # is a key, and the words that follows that pair are the keys.
+
 def build_markov(corpus, state_size):
     corpus = corpus.split()#split the string input into a list of words
     markov = {}#create an empty dictionary to hold the model - keys and values are words
@@ -29,9 +29,9 @@ def build_markov(corpus, state_size):
     return markov
 
 
-# This function takes as input a Markov model (in the form of a dictionary), the state size, and the minimum number of words you want to generate.
-# It returns a string that (hopefully) sounds like the text input into the model! 
-def generate_text(model, state_size, min_length):
+# This function takes as input a Markov model (in the form of a dictionary), the state size, and the maximum number of words you want to generate.
+# It returns a string that (hopefully) sounds like the text input into the model!
+def generate_text(model, state_size, max_length):
 
     #P
     def get_new_starter():
@@ -49,17 +49,17 @@ def generate_text(model, state_size, min_length):
         next_word = random.choice(model[key])
         text.append(next_word)
         i += 1
-        if i > min_length and text[-1][-1] == '.':
+        if i == max_length:
             break
     return ' '.join(text)
 
 # This is like "main" in java - here is where we run the program by calling the functions
 if __name__ == "__main__":
     body = input('drake.txt')
-    print(body)
-    model = build_markov(body,10)
-    print(model)
-    print(generate_text(model,10,100))
+    # print(body)
+    model = build_markov(body,2)
+    # print(model)
+    print(generate_text(model,2,100))
     # body = input('beatles.txt')
     # print(body)
     # model = build_markov(body,2)
